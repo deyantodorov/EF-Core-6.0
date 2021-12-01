89fbb51467fbd9c0e8d33e45eb485b72589ce4d3
Deyan Todorov <deyan.todorov@endava.com>
2021-12-01 09:58:32 +0200

    init structure

---
+++ EfCore6.0/EfCore.Domain/Team.cs
@@ -0,0 +1,13 @@
+﻿namespace EfCore.Domain
+{
+    public class Team
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int LeagueId { get; set; }
+
+        public virtual League League { get; set; }
+    }
+}
