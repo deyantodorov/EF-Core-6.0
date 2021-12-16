880c040e8934e78f9c0e2fbbf2c5036a01a3c03e
Deyan Todorov <deyan.todorov@endava.com>
2021-12-16 16:22:04 +0200

    add EF core layer for SqlServer

---
+++ EfCore.Data.SqlServer/SqlServerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EfCore.Domain;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCore.Data.SqlServer
+{
+    public class SqlServerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433; Database=FootballLeague; User Id=sa; Password=P@ssword123;");
+        }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<League> Leagues { get; set; }
+    }
+}
