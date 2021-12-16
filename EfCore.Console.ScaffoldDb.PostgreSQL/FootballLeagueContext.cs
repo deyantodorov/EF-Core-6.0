bfe722fea7bc99633b74deb07ac7f3d4065271e8
Deyan Todorov <deyan.todorov@endava.com>
2021-12-16 18:13:51 +0200

    scaffold of postgresql

---
+++ EfCore.Console.ScaffoldDb.PostgreSQL/FootballLeagueContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EfCore.Console.ScaffoldDb.PostgreSQL
+{
+    public partial class FootballLeagueContext : DbContext
+    {
+        public FootballLeagueContext()
+        {
+        }
+
+        public FootballLeagueContext(DbContextOptions<FootballLeagueContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<League> Leagues { get; set; } = null!;
+        public virtual DbSet<Team> Teams { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Username=postgres;Password=secretpassword;Host=127.0.0.1;Port=5432;Database=FootballLeague;Pooling=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>(entity =>
+            {
+                entity.HasIndex(e => e.LeagueId, "IX_Teams_LeagueId");
+
+                entity.HasOne(d => d.League)
+                    .WithMany(p => p.Teams)
+                    .HasForeignKey(d => d.LeagueId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
