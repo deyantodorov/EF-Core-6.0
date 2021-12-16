8182f814781a8dde6a1575214ac71d2c2dc81bbe
Deyan Todorov <deyan.todorov@endava.com>
2021-12-16 16:22:24 +0200

    add EF core layer for PostgreSQL

---
+++ EfCore.Data.PostgreSQL/PostgreSqlDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EfCore.Domain;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCore.Data.PostgreSQL
+{
+    public class PostgreSqlDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Username=postgres;Password=secretpassword;Host=127.0.0.1;Port=5432;Database=FootballLeague;Pooling=true;");
+        }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<League> Leagues { get; set; }
+    }
+}
